5426d5b02b36fd553de48b14b458abf339b9710a
root <root@a122171.nptu.edu.tw>
2021-02-16 21:31:07 +0800

    Edit 1/2

--- ComityRT_Create.py
+++ ComityRT_Create.py
@@ -2,25 +2,32 @@ import configparser
 import inquirer
 import os
 config = configparser.ConfigParser()
-config.read('ComityRT.ini')
 
-Cirityical_Name = config.get('ComityRT' , 'Cirityical_Name').split()
+Config_MainList=["Cirityical_Name","CPU_Use","Ciritical_Level_Count","Pirority_algorithm","CPU_Limit","Workload_Name","Execution_Level_Mode","Priority_Mode","Scheduleability_analysis","Back"]
 
-CPU_Use = config.get('ComityRT' , 'CPU_Use').split()
+Config_ConList=["CPU_Limit","CPU_Use","CPU_Weights","Memory_Limit","Back"]
 
-Ciritical_Level_Count=config['ComityRT']['Ciritical_Level_Count']
+Cirityical_Name =""
 
-Pirority_algorithm =config['ComityRT']['Pirority_algorithm']
+CPU_Use =""
 
-CPU_Limit=config['ComityRT']['CPU_Limit']
+Ciritical_Level_Count =""
 
-Workload_Name = config['ComityRT']['Workload_Name']
+Pirority_algorithm =""
 
-Execution_Level_Mode = config['ComityRT']['Execution_Level_Mode']
+CPU_Limit=""
 
-Priority_Mode = config['ComityRT']['Priority_Mode']
+Workload_Name =""
 
-Scheduleability_analysis = config['ComityRT']['Scheduleability_analysis']
+Execution_Level_Mode =""
+
+Priority_Mode =""
+
+Scheduleability_analysis =""
+
+key=""
+option=""
+
 def Show_Welcom():
   print('\n================ ComityRT-Control =================\n')
 	
@@ -32,20 +39,85 @@ def Choose_List():
   questions = [
   inquirer.List('action',
                 message="Choose An Action",
-                choices=['(1)Build system through configuration file', '(2)Edit system configuration file', '(3)System running', '(4)System Stoping'],
+                choices=['(1)Build system through configuration file', '(2)Edit system configuration file', '(3)System Start', '(4)System Stop' ,'(5)View ComityRT Log'],
             ),
   ]
   answers = inquirer.prompt(questions)
   if answers['action']=="(1)Build system through configuration file":
-    Bulid_system()
+    Bulid_system(config)
   elif answers['action']=="(2)Edit system configuration file":
-    print("2")
-  elif answers['action']=="(3)System running":
+    Edit_config()
+  elif answers['action']=="(3)System Start":
     print("3")
-  elif answers['action']=="(4)System Stoping":
+  elif answers['action']=="(4)System Stop":
     print("4")
+  elif answers['action']=="(5)View ComityRT Log":
+    print("5")
 
-def Bulid_system():
+def Choose_config_option(AList):
+  questions = [
+  inquirer.List('option',
+                message="Choose An option",
+                choices=AList,
+            ),
+  ]
+
+  answers = inquirer.prompt(questions)
+  return answers['option']
+
+def Choose_config_Mkey():
+  questions = [
+  inquirer.List('action',
+                message="Choose a parameter to modify",
+                choices=Config_MainList,
+            ),
+  ]
+  answers = inquirer.prompt(questions)
+  return answers['action']
+
+def Choose_config_Ckey():
+  questions = [
+  inquirer.List('action',
+                message="Choose a parameter to modify",
+                choices=Config_ConList,
+            ),
+  ]
+  answers = inquirer.prompt(questions)
+  return answers['action']
+
+def Choose_edit_config(config,AList):
+  global option
+  global key
+
+  option=Choose_config_option(AList)
+  if option=="ComityRT":
+    key=Choose_config_Mkey()
+  elif option=="Back":
+    Edit_config()
+  else:
+    key=Choose_config_Ckey()
+
+  if key=="Back":
+    Choose_edit_config(config,AList)
+  
+  
+def Edit_config():
+  config = configparser.ConfigParser()
+  config_name=Load_config(config) 
+  path="./config/"+config_name
+  config.read(path)
+  global option
+  global key
+  AList=config.sections()
+  AList.append("Back")
+  Choose_edit_config(config,AList)
+
+  print(option+" "+key)
+  
+def Bulid_system(config):
+  config_name=Load_config(config)
+
+def Load_config(config):
   choices=[]
   path="./config"
   if os.path.isdir(path):
@@ -63,11 +135,63 @@ def Bulid_system():
   ]
   answers = inquirer.prompt(questions)
   
+  configpath='./config/'+answers['action']
+  
+  config.read(configpath)
+  global Cirityical_Name
+  global CPU_Use
+  global Ciritical_Level_Count
+  global Pirority_algorithm
+  global CPU_Limit
+  global Workload_Name
+  global Execution_Level_Mode
+  global Priority_Mode
+  global Scheduleability_analysis
+
+  Cirityical_Name = config.get('ComityRT' , 'Cirityical_Name').split()
+
+  CPU_Use = config.get('ComityRT' , 'CPU_Use').split()
+
+  Ciritical_Level_Count=config['ComityRT']['Ciritical_Level_Count']
+
+  Pirority_algorithm =config['ComityRT']['Pirority_algorithm']
+
+  CPU_Limit=config['ComityRT']['CPU_Limit']
+
+  Workload_Name = config['ComityRT']['Workload_Name']
+
+  Execution_Level_Mode = config['ComityRT']['Execution_Level_Mode']
+
+  Priority_Mode = config['ComityRT']['Priority_Mode']
+
+  Scheduleability_analysis = config['ComityRT']['Scheduleability_analysis']
+  
+  fname=answers['action']
+  
+  View_parameters(fname)
+
+  answers = ContinueQue()
+ 
+  if answers['continue']==True:
+    return fname
+  else:
+    Load_config(config)
+
+def ContinueQue():
+  questions = [
+    inquirer.Confirm('continue',
+                  message="Choose this configuration file?"),
+  ]
+  answers = inquirer.prompt(questions)
+  return answers
 
 def ComityRT_Status():
   print('he')  
 
-def View_parameters():
+def View_parameters(fname):
+  
+  print('\n================ '+fname+' =================\n')
+
   print('Ciritical_Level_Count =',Ciritical_Level_Count)
   print('\nCirityical_Name =',Cirityical_Name)
   print('\nPirority_algorithm =',Pirority_algorithm)
@@ -77,6 +201,8 @@ def View_parameters():
   print('\nExecution_Level_Mode =',Execution_Level_Mode)
   print('\nPriority_Mode =',Priority_Mode)
   print('\nScheduleability_analysis =',Scheduleability_analysis)
+
+  print('\n===============================================\n')
 
 Show_Welcom()
 
